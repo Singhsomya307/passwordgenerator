f181bf0ec5c6790258adab30b1471fcdb396f1c3
Singhsomya307 <104775940+Singhsomya307@users.noreply.github.com>
2024-05-31 19:48:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+/* 
+let currDate= document.getElementById("currDate");
+let dateOfBirth = document.querySelector("#DOB");
+const CalcAge= document.getElementById("CalcAge");
+const displayAge= document.getElementById("displayAge");
+const Age= document.getElementById("age");
+var today = new Date();
+currDate.innerText=`Today's Date is : ${today.toLocaleDateString('en-US')}`;
+
+CalcAge.addEventListener("click",()=>{
+    var birthDate = new Date(dateOfBirth.value);
+    var age = today.getFullYear() - birthDate.getFullYear();
+    var m = today.getMonth() - birthDate.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
+        age = age - 1;
+    }
+displayAge.style.visibility="visible";
+Age.innerText=`You are ${age} years old.`
+});
+*/
+
+
+function shuffle(string) {
+    let array = string.split('');
+    for (let i = array.length - 1; i > 0; i--) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
+    return array.join('');
+}
+
+function generatePassword() {
+    let length = document.getElementById('length').value;
+    let passwordDisplay = document.getElementById('password');
+
+    if (length < 4) {
+        passwordDisplay.textContent = "Password length should be at least 4 characters.";
+        return;
+    }
+
+    const uppercaseLetter = String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+    const lowercaseLetter = String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+    const digit = String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+    const specialCharacters = "!@#$%^&*()_+[]{}|;:,.<>?";
+    const specialCharacter = specialCharacters.charAt(Math.floor(Math.random() * specialCharacters.length));
+
+    let remainingLength = length - 4;
+    let allCharacters = uppercaseLetter + lowercaseLetter + digit + specialCharacter;
+    const possibleCharacters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789' + specialCharacters;
+
+    for (let i = 0; i < remainingLength; i++) {
+        allCharacters += possibleCharacters.charAt(Math.floor(Math.random() * possibleCharacters.length));
+    }
+
+    let password = shuffle(allCharacters);
+    passwordDisplay.textContent = "Generated password: " + password;
+}
