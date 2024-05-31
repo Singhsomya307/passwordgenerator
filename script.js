a8537a2682bbcd6759aaf6d2bf81c2d331728815
Singhsomya307 <104775940+Singhsomya307@users.noreply.github.com>
2024-05-31 19:57:24 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-/* 
-let currDate= document.getElementById("currDate");
-let dateOfBirth = document.querySelector("#DOB");
-const CalcAge= document.getElementById("CalcAge");
-const displayAge= document.getElementById("displayAge");
-const Age= document.getElementById("age");
-var today = new Date();
-currDate.innerText=`Today's Date is : ${today.toLocaleDateString('en-US')}`;
-
-CalcAge.addEventListener("click",()=>{
-    var birthDate = new Date(dateOfBirth.value);
-    var age = today.getFullYear() - birthDate.getFullYear();
-    var m = today.getMonth() - birthDate.getMonth();
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-        age = age - 1;
-    }
-displayAge.style.visibility="visible";
-Age.innerText=`You are ${age} years old.`
-});
-*/
-
 
 function shuffle(string) {
     let array = string.split('');
@@ -55,3 +34,4 @@ function generatePassword() {
     let password = shuffle(allCharacters);
     passwordDisplay.textContent = "Generated password: " + password;
 }
+
